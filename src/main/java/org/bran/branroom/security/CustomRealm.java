f9580835d82952cfcef0d6d94a5889cf01c5cc7d
BranSummer <bransummer524@gmail.com>
2018-07-19 23:22:11 +0800

    shiro配置

---
+++ src/main/java/org/bran/branroom/security/CustomRealm.java
@@ -0,0 +1,23 @@
+package org.bran.branroom.security;
+
+import org.apache.shiro.authc.AuthenticationException;
+import org.apache.shiro.authc.AuthenticationInfo;
+import org.apache.shiro.authc.AuthenticationToken;
+import org.apache.shiro.authz.AuthorizationInfo;
+import org.apache.shiro.realm.AuthorizingRealm;
+import org.apache.shiro.subject.PrincipalCollection;
+
+/**
+ * realm是用来验证用户是否输入提交了正确的信息，并获取用户的角色和权限，以便后面对用户的角色和权限做相应的判断。
+ */
+public class CustomRealm extends AuthorizingRealm {
+    @Override
+    protected AuthorizationInfo doGetAuthorizationInfo(PrincipalCollection principalCollection) {
+        return null;
+    }
+
+    @Override
+    protected AuthenticationInfo doGetAuthenticationInfo(AuthenticationToken authenticationToken) throws AuthenticationException {
+        return null;
+    }
+}
